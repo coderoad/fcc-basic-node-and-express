fc7c42b76232a9ac0bc675ac2c4d0080624a761d
shmck <shawn.j.mckay@gmail.com>
2020-03-17 20:22:43 -0700

    6.3 set MESSAGE_STYLE as uppercase

--- coderoad/test/env.test.js
+++ coderoad/test/env.test.js
@@ -7,8 +7,8 @@ const stat = util.promisify(fs.stat);
 const readFile = util.promisify(fs.readFile);
 
 describe(".env", () => {
+  const filePath = path.join(__dirname, "..", "..", ".env");
   it("should have a file", async () => {
-    const filePath = path.join(__dirname, "..", "..", ".env");
     const hasEnv = await stat(filePath).catch((err) => {
       if (err) {
         console.error(err);
@@ -30,5 +30,17 @@ describe(".env", () => {
       }
     }
     assert.ok(hasMatch, ".env is not added to the .gitignore file");
+  });
+  // 6.3
+  it('should have the key MESSAGE_STYLE with a value "uppercase"', async () => {
+    const envFile = await readFile(filePath, "utf8");
+    const lines = envFile.split("\n");
+    let hasMatch = false;
+    for (const line of lines) {
+      if (line.match(/^MESSAGE_STYLE/)) {
+        hasMatch = !!line.match(/(\"\')?uppercase(\"\')?/);
+      }
+    }
+    assert.ok(hasMatch, 'MESSAGE_STYLE should equal "uppercase"');
   });
 });
